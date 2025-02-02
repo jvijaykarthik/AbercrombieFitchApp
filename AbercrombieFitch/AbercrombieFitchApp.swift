1299794107d072bb2b2b7085b48529243d80db3f
VIJAY KARTHIK JEYARAJ <jvijaykarthik@yahoo.com>
2025-02-01 22:07:01 -0500

    Initial Commit

---
+++ AbercrombieFitch/AbercrombieFitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbercrombieFitchApp.swift
+//  AbercrombieFitch
+//
+//  Created by Deepika Robin on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AbercrombieFitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
